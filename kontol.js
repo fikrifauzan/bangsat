5ece14cf5fe414eb29d2a9f2292f931177dbce93
fikrifauzan <fikrifauzan262@gmail.com>
2019-06-29 13:19:37 +0700

    Update kontol.js

--- kontol.js
+++ kontol.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://www.punteuncoy.online/p/asdasdasd.html#?o=" + encryptedUrl;
+            a_to_ve[i].href = "https://www.punteuncoy.online/p/5-basic-facts-about-health-insurance.html#?o=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
